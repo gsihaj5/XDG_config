a9f565bb3cc944748a6f558262834a3f035203db
gerry sihaj <gsihaj5@gmail.com>
2022-02-23 10:09:31 +0700

    feat: add nvim config with lua file

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,57 @@
+-- mapping function to connect to vim api
+function map(mode, shortcut, command)
+	vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true})
+end
+
+function nmap(shortcut, command)
+	map('n', shortcut, command)
+end
+
+function imap(shortcut, command)
+	map('i', shortcut, command)
+end
+
+function vmap(shortcut, command)
+	map('v', shortcut, command)
+end
+
+function cmap(shortcut, command)
+	map('c', shortcut, command)
+end
+
+function tmap(shortcut, command)
+	map('t', shortcut, command)
+end
+
+-- THE MAPPINGS
+
+-- keep search result in the midle of window
+nmap('n', 'nzzzv')
+nmap('N', 'Nzzzv')
+
+
+-- WINDOWS
+-- opening new window
+-- split horizontal
+nmap('sh', ':sp<cr>')
+-- split vertical
+nmap('sv', ':vs<cr>')
+
+-- window navigation
+nmap('<C-h>', '<C-w>h')
+nmap('<C-j>', '<C-w>j')
+nmap('<C-k>', '<C-w>k')
+nmap('<C-l>', '<C-w>l')
+
+-- window resize
+nmap('<S-k>', ':res -1<cr>')
+nmap('<S-j>', ':res +1<cr>')
+nmap('<S-h>', ':vert res -1<cr>')
+nmap('<S-l>', ':vert res +1<cr>')
+
+-- TABS
+nmap('<C-n><C-t>', ':tabnew<cr>')
+nmap('<C-c><C-t>', ':tabclose<cr>')
+
+nmap('n<Tab>', ':tabn<cr>')
+nmap('p<Tab>', ':tabp<cr>')
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,3 @@
+vim.o.ruler = true
+vim.o.showcmd = true
+vim.o.showmode = true

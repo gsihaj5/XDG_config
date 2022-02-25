311c91efe5369f49199887ae8932997e6f31d66c
gerry sihaj <gsihaj5@gmail.com>
2022-02-25 12:33:29 +0700

    expand tab to false

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -15,10 +15,10 @@ vim.o.relativenumber = true --relative number based on cursor pos
 -- White characters
 vim.o.autoindent = true
 vim.o.smartindent = true
-vim.o.tabstop = 2 -- 1 tab = 2 spaces
+vim.o.tabstop = 4 -- 1 tab = 2 spaces
 vim.o.shiftwidth = 4 -- indentation rule
 vim.o.formatoptions = 'qnj1' -- q  - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
-vim.o.expandtab = true -- expand tab to spaces
+vim.o.expandtab = false -- expand tab to spaces
 
 vim.cmd("syntax on")
 

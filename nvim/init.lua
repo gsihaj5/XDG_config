e0d3f8d1b8f106709b130ab0573a582577c42b9b
gerry sihaj <gsihaj5@gmail.com>
2022-02-25 21:56:02 +0700

    alter autocommand to load coc for blade only

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,17 +9,6 @@ require('lsp.sumneko_lua')
 require('lsp.intelephense')
 require('lsp.tailwindcss')
 
-local autocmd = require('au')
-
-require('plugins-config.builtin-lsp')
-autocmd.BufEnter = {
-    '*.blade.php',
-    function()
-		vim.cmd("source $HOME/.config/nvim/lua/plugins-config/coc.vim")
-    end,
-}
-
-
 --this is basic settings
 require('settings')
 require('mappings')
--- nvim/lua/plugins-settings.lua
+++ nvim/lua/plugins-settings.lua
@@ -3,4 +3,19 @@ vim.cmd('let g:airline_powerline_fonts = 1')
 --nerd tree
 vim.cmd('nnoremap <space>n :NERDTreeToggle<CR>')
 
-require('plugins-config.nvim-cmp')
+local autocmd = require('au')
+
+autocmd.BufEnter = { '*', function()
+		local filetype = vim.bo.filetype
+		if filetype == 'blade' then
+			print("coc")
+			vim.cmd("source $HOME/.config/nvim/lua/plugins-config/coc.vim")
+		else
+			print("builtin lsp")
+			require('plugins-config.nvim-cmp')
+			require('plugins-config.builtin-lsp')
+		end
+    end,
+}
+
+

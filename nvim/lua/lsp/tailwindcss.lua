f76d7dd62e864cb09ceccc1743a37c0fd811a967
gerry sihaj <gsihaj5@gmail.com>
2022-02-26 18:07:21 +0700

    fix relative path

--- nvim/lua/lsp/tailwindcss.lua
+++ nvim/lua/lsp/tailwindcss.lua
@@ -1,7 +1,7 @@
 local capabilities = require('cmp_nvim_lsp')
   .update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local tailwind_bin_folder = "/home/gerry/.local/share/nvim/lsp_servers/tailwindcss_npm/node_modules/@tailwindcss/language-server/bin/"
+local tailwind_bin_folder = "~/.local/share/nvim/lsp_servers/tailwindcss_npm/node_modules/@tailwindcss/language-server/bin/"
 local tailwind_binary_path = tailwind_bin_folder .. "tailwindcss-language-server"
 local tailwind_main_path = tailwind_bin_folder .. "main.lua"
 

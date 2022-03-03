c2e4b34b88443f2a5e33405836edd310a6a7b78d
gerry sihaj <gsihaj5@gmail.com>
2022-03-03 12:26:45 +0700

    chore: remove print

--- nvim/lua/plugins-settings.lua
+++ nvim/lua/plugins-settings.lua
@@ -10,10 +10,8 @@ local autocmd = require('au')
 autocmd.BufEnter = { '*', function()
 		local filetype = vim.bo.filetype
 		if filetype == 'blade' then
-			print("coc")
 			vim.cmd("source $HOME/.config/nvim/lua/plugins-config/coc.vim")
 		else
-			print("builtin lsp")
 			require('plugins-config.nvim-cmp')
 			require('plugins-config.builtin-lsp')
 		end

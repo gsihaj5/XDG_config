bae14d593ec47b88d45678d4efe9090d24cdc3e6
gerry sihaj <gsihaj5@gmail.com>
2022-05-29 13:46:37 +0700

    feat: enable php formatting

--- nvim/lua/lsp/intelephense.lua
+++ nvim/lua/lsp/intelephense.lua
@@ -2,8 +2,8 @@ local capabilities = require('cmp_nvim_lsp')
   .update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 return require('lspconfig').intelephense.setup{
-    capabilities = capabilities,
-  	on_attach = function(client)
-    	client.resolved_capabilities.document_formatting = false
-  	end,
+    capabilities = capabilities
+      --on_attach = function(client)
+        --client.resolved_capabilities.document_formatting = false
+      --end,
 }

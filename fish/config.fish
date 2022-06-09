09c948e8f6b94996fd4dbc7dec7cbdc439ad51d7
gerry sihaj <gsihaj5@gmail.com>
2022-06-09 08:30:27 +0700

    fix: add utf 8 environment on fish

--- fish/config.fish
+++ fish/config.fish
@@ -44,6 +44,7 @@ set -g fish_prompt_pwd_dir_length 0
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor yes
 set -g theme_newline_prompt '$ '
+set -x LC_ALL en_US.UTF-8
 
 function gs
 	git status

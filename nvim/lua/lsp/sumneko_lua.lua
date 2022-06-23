2ab0e94b8ab365b5265ec3acefd27cea24df8a8b
gerry sihaj <gsihaj5@gmail.com>
2022-06-23 23:11:01 +0700

    chore: formatting

--- nvim/lua/lsp/sumneko_lua.lua
+++ nvim/lua/lsp/sumneko_lua.lua
@@ -7,25 +7,37 @@ local capabilities = require('cmp_nvim_lsp')
 return require('lspconfig').sumneko_lua.setup{
     capabilities = capabilities;
     cmd = {sumneko_binary_path, "-E", sumneko_main_path};
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = vim.split(package.path, ';'),
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-        },
-      },
-    },
-  },
+  	settings = {
+    	Lua = {
+			format = {
+    			enable = true,
+    			-- Put format options here
+    			-- NOTE: the value should be STRING!!
+    			defaultConfig = {
+      				indent_style = "space",
+      				indent_size = "2",
+    			}
+  			},
+    		runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        		version = 'LuaJIT',
+        		-- Setup your lua path
+        		path = vim.split(package.path, ';'),
+			},
+    		diagnostics = {
+        		-- Get the language server to recognize the `vim` global
+        		globals = {'vim'},
+    		},
+    		workspace = {
+    			-- Make the server aware of Neovim runtime files
+        		library = {
+          			[vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          			[vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        		},
+				neededFileStatus = {
+      				["codestyle-check"] = "Any",
+    			},
+    		},
+		},
+	},
 }

3a1a8ce080757cee4d1b8f41460b38e248422ec4
laptop windows <gsihaj5@gmail.com>
2023-05-26 12:36:40 +0700

    add git control for push and diff

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gc", function() vim.cmd.Git("commit") end)
+vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git("push") end)
+vim.keymap.set("n", "<leader>gq", function() vim.cmd.Git("diff") end)

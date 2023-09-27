54979df556e0a939e8124c989009efc72038512b
gerry from ryzen <gsihaj5@gmail.com>
2023-09-27 12:04:42 +0700

    fix: mapping

--- nvim/lua/gsihaj5/mappings.lua
+++ nvim/lua/gsihaj5/mappings.lua
@@ -33,8 +33,8 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<C-l>", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
@@ -50,4 +50,4 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
-vim.keymap.set({ "n", "i" }, "<Tab>", "<Tab>")
+-- vim.keymap.set({ "n", "i" }, "<Tab>", "<Tab>")

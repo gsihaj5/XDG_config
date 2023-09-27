001ad9404e84c041f0b0f9d8bbc9374ec27578cd
gerry from ryzen <gsihaj5@gmail.com>
2023-09-27 09:21:04 +0700

    fix: webdevicons opt type

--- nvim/lua/gsihaj5/plugins.lua
+++ nvim/lua/gsihaj5/plugins.lua
@@ -36,7 +36,7 @@ return require('packer').startup(function()
     --status bar
     use {
         'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+        requires = { 'nvim-tree/nvim-web-devicons'}
     }
 
     use {

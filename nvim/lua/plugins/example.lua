02840d3349a60739945c5a1c56adcf4acc04d9fc
Gerry on MSI <gsihaj5@gmail.com>
2024-02-03 11:44:57 +0700

    add ddashboard vim

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -173,7 +173,7 @@ return {
   },
 
   -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+  { import = "lazyvim.plugins.extras.ui.dashboard" },
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },

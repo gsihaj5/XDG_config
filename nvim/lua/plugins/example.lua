d193e7586719d6b9ecdbd1def7028294eb6a4239
Gerry on MSI <gsihaj5@gmail.com>
2024-02-03 11:49:29 +0700

    customize mini starter

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -173,7 +173,7 @@ return {
   },
 
   -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.dashboard" },
+  { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },

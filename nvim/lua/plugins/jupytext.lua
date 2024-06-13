0f4440454c87a962fe79f317139004747befdc5f
gerry from ryzen <gsihaj5@gmail.com>
2024-06-13 08:10:56 +0700

    add jupytext extension

---
+++ nvim/lua/plugins/jupytext.lua
@@ -0,0 +1,9 @@
+return {
+  "GCBallesteros/jupytext.vim",
+  dependencies = {
+    "hkupty/iron.nvim",
+    "kana/vim-textobj-user",
+    "kana/vim-textobj-line",
+    "GCBallesteros/vim-textobj-hydrogen",
+  },
+}

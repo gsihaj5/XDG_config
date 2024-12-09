92e141a6ce3b71ea6270428b4f46fa625077c959
gerry from ryzen <gsihaj5@gmail.com>
2024-12-09 19:18:14 +0700

    java setup

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+--
+--
+
+require("lspconfig").jdtls.setup {
+  root_dir = function(fname)
+    return require("lspconfig").util.root_pattern("pom.xml", "gradle.build", ".git")(fname) or vim.fn.getcwd()
+  end,
+}

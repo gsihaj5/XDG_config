fa7d81aad15081e5893831ac55234a3f53f0c46e
gerry from ryzen <gsihaj5@gmail.com>
2025-01-09 23:42:53 +0700

    add celular automaton game

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,10 +17,10 @@ vim.keymap.set("n", "k", "gk")
 vim.keymap.set("n", "<leader>ww", ":set wrap!<CR>")
 
 vim.keymap.set("n", "<leader>vwm", function()
-  require("vim-with-me").StartVimWithMe()
+    require("vim-with-me").StartVimWithMe()
 end)
 vim.keymap.set("n", "<leader>svwm", function()
-  require("vim-with-me").StopVimWithMe()
+    require("vim-with-me").StopVimWithMe()
 end)
 
 -- greatest remap ever
@@ -38,14 +38,14 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<C-l>", function()
-  local clientname = ""
-  vim.lsp.buf.format({
-    filter = function(client)
-      clientname = client.name
-      return client.name ~= "tsserver"
-    end,
-  })
-  print("formating using", clientname)
+    local clientname = ""
+    vim.lsp.buf.format({
+        filter = function(client)
+            clientname = client.name
+            return client.name ~= "tsserver"
+        end,
+    })
+    print("formating using", clientname)
 end)
 
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -58,6 +58,7 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
+vim.keymap.set("n", "<leader>mgol", "<cmd>CellularAutomaton game_of_life<CR>")
 
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 

0c2ded3a5f4b1e4832e860852cb63c52e4e5c901
gerry from ryzen <gsihaj5@gmail.com>
2025-01-09 23:42:46 +0700

    add xml format checker for jdtls

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,8 +5,25 @@
 --
 --
 
-require("lspconfig").jdtls.setup {
-  root_dir = function(fname)
-    return require("lspconfig").util.root_pattern("pom.xml", "gradle.build", ".git")(fname) or vim.fn.getcwd()
-  end,
-}
+require("lspconfig").jdtls.setup({
+    root_dir = function(fname)
+        return require("lspconfig").util.root_pattern("pom.xml", "gradle.build", ".git")(fname) or vim.fn.getcwd()
+    end,
+    settings = {
+        java = {
+            format = {
+                settings = {
+                    -- url = function()
+                    --     local root_dir = require("jdtls.setup").find_root(".git")
+                    --     print("searching code_style.xml", root_dir)
+                    --     if root_dir == nil then
+                    --         return ''
+                    --     else
+                    --         return root_dir .. '/code_style.xml'
+                    --     end
+                    -- end
+                }
+            }
+        }
+    },
+})

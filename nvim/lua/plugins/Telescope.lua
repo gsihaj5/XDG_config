972534fbb216683d81bda2ba8d45c4d5b11fb7d3
gerry from ryzen <gsihaj5@gmail.com>
2025-01-23 08:51:20 +0700

    updates and disable context for fzfz

--- nvim/lua/plugins/Telescope.lua
+++ nvim/lua/plugins/Telescope.lua
@@ -19,10 +19,10 @@ return {
     },
   },
   dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
+    -- "nvim-telescope/telescope-fzf-native.nvim",
+    -- build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    -- config = function()
+    --   require("telescope").load_extension("fzf")
+    -- end,
   },
 }
--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -24,19 +24,6 @@ return {
         keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
         config = true,
     },
-
-    -- add telescope-fzf-native
-    {
-        "telescope.nvim",
-        dependencies = {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
-            config = function()
-                require("telescope").load_extension("fzf")
-            end,
-        },
-    },
-
     -- add any tools you want to have installed below
     {
         "williamboman/mason.nvim",

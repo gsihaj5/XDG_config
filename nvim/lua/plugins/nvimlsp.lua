ee0cc2533987d185f250c8a03605924bed021eb8
gerry from ryzen <gsihaj5@gmail.com>
2025-01-23 07:55:57 +0700

    updates

--- nvim/lua/plugins/nvimlsp.lua
+++ nvim/lua/plugins/nvimlsp.lua
@@ -1,6 +1,36 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    autoformat = false,
-  },
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "jose-elias-alvarez/typescript.nvim",
+        init = function()
+            require("lazyvim.util").lsp.on_attach(function(_, buffer)
+                -- stylua: ignore
+                vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports",
+                    { buffer = buffer, desc = "Organize Imports" })
+                vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+            end)
+        end,
+    },
+    ---@class PluginLspOpts
+    opts = {
+        autoformat = false,
+        ---@type lspconfig.options
+        servers = {
+            tsserver = {},
+            pyright = {},
+            jdtls = {},
+        },
+        -- you can do any additional lsp server setup here
+        -- return true if you don't want this server to be setup with lspconfig
+        ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+        setup = {
+            -- example to setup with typescript.nvim
+            tsserver = function(_, opts)
+                require("typescript").setup({ server = opts })
+                return true
+            end,
+            -- Specify * to use this function as a fallback for any server
+            -- ["*"] = function(server, opts) end,
+        },
+    },
 }

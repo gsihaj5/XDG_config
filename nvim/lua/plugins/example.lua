76d5efa8d9266b5451ed2897c2d753d935eea0a8
Gerry on MSI <gsihaj5@gmail.com>
2025-04-21 19:58:35 +0700

    remove extras, and automatically install some lsp

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -1,44 +1,30 @@
 return {
-    -- add gruvbox
-    { "ellisonleao/gruvbox.nvim" },
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
 
-    -- Configure LazyVim to load gruvbox
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "gruvbox",
-        },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
     },
+  },
 
-    -- change trouble config
-    {
-        "folke/trouble.nvim",
-        -- opts will be merged with the parent spec
-        opts = { use_diagnostic_signs = true },
-    },
+  -- change trouble config
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
 
-    -- add symbols-outline
-    {
-        "simrat39/symbols-outline.nvim",
-        cmd = "SymbolsOutline",
-        keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-        config = true,
-    },
-    -- add any tools you want to have installed below
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "intelephense",
-                "stylua",
-                "shellcheck",
-                "shfmt",
-                "flake8",
-            },
-        },
-    },
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
 
-    { 'theprimeagen/harpoon' },
-    { 'mbbill/undotree' },
-
+  { "theprimeagen/harpoon" },
+  { "mbbill/undotree" },
 }
---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,22 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      -- php
+      "intelephense",
+      -- typescript javascript
+      "eslint",
+      "eslint_d",
+      "vtsls",
+      "prettier",
+      "vtsls",
+
+      -- opther language
+      "stylua",
+      "shellcheck",
+      "shfmt",
+      "flake8",
+      "flake8",
+    },
+  },
+}

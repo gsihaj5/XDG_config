6e5301acec4501471eeb142725c47b306a4e11cf
gerryx390 <gsihaj5@gmail.com>
2025-12-11 09:54:03 +0700

    update

--- nvim/lua/plugins/sniper-generator.lua
+++
@@ -1,8 +0,0 @@
-return {
-  dir = "~/projects/sniper-generator/",
-  name = "sniper-generator",
-  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  config = function()
-    require("sniper-generator").setup()
-  end,
-}

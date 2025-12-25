092ee52042ed71181f80fb42a39f421bcbbd631c
gerryx390 <gsihaj5@gmail.com>
2025-12-25 13:49:59 +0700

    fix: wrong options for render-markdown

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,5 +1,10 @@
-require("render-markdown").setup({
-  -- render_modes = { "n", "c", "t" },
-  render_modes = true,
-  anti_conceal = { enabled = true },
-})
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  config = function()
+    require("render-markdown").setup({
+      -- render_modes = { "n", "c", "t" },
+      render_modes = true,
+      anti_conceal = { enabled = true },
+    })
+  end,
+}
